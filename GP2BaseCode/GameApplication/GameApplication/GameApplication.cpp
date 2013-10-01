17da15b14e2f225fee843ac9b545f487295377aa
PWATT200 <pdapdaPWATT200@caledonian.ac.uk>
2013-10-01 10:42:49 +0100

    CGameApplication Completion

--- GP2BaseCode/GameApplication/GameApplication/GameApplication.cpp
+++ GP2BaseCode/GameApplication/GameApplication/GameApplication.cpp
@@ -22,5 +22,98 @@ CGameApplication::CGameApplication(void)
 //desconstructor
 CGameApplication::~CGameApplication(void)
 {
-	// delete 
+	// delete things in reverse order
+	if(m_pRenderer)
+	{
+		delete m_pRenderer;
+		m_pRenderer = NULL;
+	}
+	if(m_pWindow)
+	{
+		delete m_pWindow;
+		m_pWindow = NULL;
+	}
+}
+
+//Init
+//This initialises all subsystems
+bool CGameApplication::init()
+{
+	if(!parseConfigFile())
+		return false;
+	if(!initWindow())
+		return false;
+	if(!initGraphics())
+		return false;
+	if(!initInput())
+		return false;
+	if(!initGame())
+		return false;
+	return true;
+}
+
+//called to parse the config file
+bool CGameApplication::parseConfigFile()
+{
+	return true;
+}
+
+//initInput - Initialises the input 
+bool CGameApplication::initInput()
+{
+	return true;
+}
+
+//initPhysics - Initialises the physics system
+bool CGameApplication::initPhysics()
+{
+	return true;
+}
+
+//initGraphics - Initialises the graphics
+bool CGameApplication::initGraphics()
+{
+	return true;
+}
+
+//initWindow - initialises the window
+bool CGameApplication::initWindow()
+{
+	//Create a Win32 Window
+	m_pWindow=new CWin32Window();
+	m_pWindow->init(m_GameOptionDesc.gameName, m_GameOptionDesc.width, m_GameOptionDesc.height, m_GameOptionDesc.fullscreen);
+	return true;
+}
+
+//called to init the game elements
+bool CGameApplication::initGame()
+{
+	return true;
+}
+
+// Called to put the gane in a loop(aka game loop)
+void CGameApplication::run()
+{
+	//while the window is not closed
+	while(m_pWindow->running())
+	{
+		//check for all windows messages
+		m_pWindow->checkForWindowMessages();
+		//update
+		update();
+		//render
+		render();
+	}	
+}
+
+//Render, called to draw one frame of the game
+void CGameApplication::render()
+{
+
+}
+
+//Update, called to update the game
+void CGameApplication::update()
+{
+	
 }
